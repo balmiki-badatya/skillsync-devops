68d0ad803c027bae80aa747472b025550b6735e3
Balmiki Badatya <balmikib830@outlook.com>
2025-10-28 22:22:40 -0700

    Feature/fix (#8)
    * updated
    
    * updated ci.yml
    
    * Update terraform config
    
    * Updated subnet config
    
    * updated count to for_each
    
    * updated index() parameters

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+data "aws_availability_zones" "available_az" {
+  state = "available"
+}
+
 module "networking" {
   source         = "./modules/networking"
   vpc_cidr       = var.vpc_cidr
   vpc_name       = var.vpc_name
   default_region = var.default_region
   default_tags   = local.common_tags
+  az_list        = toset(data.aws_availability_zones.available_az.names)
 }
