8ca418557ca4f6966b761d8f200dbe4baed9b7ae
balmiki-badatya <balmikib830@outlook.com>
2025-10-28 23:09:12 -0700

    Updated code added locals

--- main.tf
+++ main.tf
@@ -8,6 +8,5 @@ module "networking" {
   vpc_name       = var.vpc_name
   default_region = var.default_region
   default_tags   = local.common_tags
-  az_list        = data.aws_availability_zones.available_az.names
-
+  az_lists       = data.aws_availability_zones.available_az.names
 }
