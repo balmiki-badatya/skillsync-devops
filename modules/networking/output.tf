47fec6619860f2c8aed4caaf3166ec927effb5cf
balmiki-badatya <balmikib830@outlook.com>
2025-10-29 00:45:40 -0700

    Added outputs

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -2,5 +2,9 @@ output "vpc_id" {
   description = "AWS vpc id"
   value       = aws_vpc.skillsync_vpc.id
   depends_on  = [aws_vpc.skillsync_vpc]
+}
 
+output "private_subnet_ids" {
+  description = "Private subnet id's"
+  value       = aws_subnet.private_subnets[*].id
 }
