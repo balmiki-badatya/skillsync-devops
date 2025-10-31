266f829ded24827dd19d8700f741fe8634386d62
balmiki-badatya <balmikib830@outlook.com>
2025-10-30 20:29:39 -0700

    fixed for_each issue

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,41 +1,12 @@
 locals {
   default_ami              = data.aws_ami.default_ami.id
-  sonar_ami_id             = var.sonar_ami != null ? var.sonar_ami : local.default_ami
+  sonar_ami_id             = var.sonar_ami
   sonar_instance_type      = var.sonar_instance_type != null ? var.sonar_instance_type : lookup(var.default_instance_type_map, "sonar", null)
-  subnet_id                = var.subnet_id != null ? var.subnet_id : data.aws_subnet.default_private_subnet.id
-  bastion_ami_id           = var.bastion_ami_id != null ? var.bastion_ami_id : local.default_ami
-  public_bastion_subnet_id = var.bastion_subnet_id != null ? var.bastion_subnet_id : data.aws_subnet.default_public_subnet.id
+  subnet_id                = var.subnet_id
+  bastion_ami_id           = var.bastion_ami_id
+  public_bastion_subnet_id = var.bastion_subnet_id
   bastion_instance_type    = var.bastion_host_instance_type != null ? var.bastion_host_instance_type : lookup(var.default_instance_type_map, "bastion", null)
 }
-
-data "aws_ami" "default_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.*-kernel-6.*-x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-  owners = ["amazon"]
-
-}
-
-data "aws_subnet" "default_private_subnet" {
-  id = "subnet-0dd64dfa23b1ee2ab"
-}
-
-data "aws_subnet" "default_public_subnet" {
-  id = "subnet-0523f968e75c2d3fd"
-}
-
 
 ############################################### SONAR server config ###############################################
 # RSA key of size 4096 bits
